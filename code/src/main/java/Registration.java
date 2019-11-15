4006fc1dc504a1c7eaf3f141b87cbca122214887
zixuan zhang <zhangzix@usc.edu>
2019-11-14 17:30:44 -0800

    skeleton project done

---
+++ code/src/main/java/Registration.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Register")
+public class Registration extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+@Override
+  public void service(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+
+    response.getWriter().print("Register\r\n");
+
+  }
+}
