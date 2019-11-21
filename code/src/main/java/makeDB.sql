48e9414f78503a8e08082a7fe9138399f5a3fee6
Stephen Pierzchala <pierzcha@usc.edu>
2019-11-20 23:01:17 -0800

    added code to create the database

---
+++ code/src/main/java/makeDB.sql
@@ -0,0 +1,88 @@
+-- This sql script sets up the database structure of our project.
+-- Be careful, running this script will delete the current data base contents!
+
+DROP DATABASE IF EXISTS RosterDB;
+
+CREATE DATABASE RosterDB;
+
+USE RosterDB; 
+
+CREATE TABLE User{
+  userID int(11) not null primary key auto_increment,
+  email varchar(20) not null,
+  password varchar(20) not null,
+  userType boolean not null
+};
+
+CREATE TABLE Student{
+  studentID int(11) not null primary key,
+  mac1 varchar(12) not null,
+  mac2 varchar(12),
+  schoolID int(11) not null,
+  name varchar(40) not null,
+  foreign key(studentID) references User(userID)
+};
+
+CREATE TABLE Instructor{
+ instructorID int(11) not null primary key,
+ name varchar(40) not null,
+ foreign key(instructorID) references User(userID)
+};
+
+CREATE TABLE Course{
+  courseID int(11) not null primary key auto_increment,
+  courseName varchar(100) not null,
+  instructorID int(11) not null,
+  numGraceDays int(2),
+  currentLectureNumber int(2) not null, --starts at 0
+  foreign key(instructorID) references Instructor(instructorID)
+};
+
+CREATE TABLE Enrollment{
+  enrollmentID int(11) not null primary key auto_increment,
+  studentID int(11) not null,
+  courseID int(11) not null,
+  foreign key(studentID) references Student(studentID),
+  foreign key(courseID) references Course(courseID)
+};
+
+
+CREATE TABLE Lecture{
+  lectureID int(11) not null primary key auto_increment,
+  courseID int(11) not null,
+  instructorID int(11) not null,
+  lectureStartTime datetime not null,
+  lectureNumber int(2) not null,
+  keyword varchar(10) not null,
+  lat float(7, 5) not null,
+  long float(8, 5) not null,
+  accuracy float(20,10) not null
+};
+
+CREATE TABLE Attendance{
+  attendanceID int(11) not null primary key auto_increment,
+  studentID int(11) not null,
+  courseID int(11) not null,
+  prevLectureRating int(1),
+  prescence boolean not null,
+  lectureNumber int(2) not null,
+  foreign key(studentID) references Student(studentID),
+  foreign key(courseID) references Course(courseID)
+};
+
+CREATE TABLE Question{
+  questionID int(11) not null primary key auto_increment,
+  content long varchar not null,
+  upvoteCount int(3) not null,
+  timeCreated datetime not null,
+  courseID int(11) not null,
+  foreign key(courseID) references Course(courseID)
+};
+
+CREATE TABLE GraceDays{
+  graceID int(11) not null primary key auto_increment,
+  userID int(11) not null,
+  numGraceDays int(2) not null,
+  courseID int(11) not null,
+  foreign key(courseID) references Course(courseID)
+};
