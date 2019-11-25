78d296a9ba7dd9fd3c624ee23c0b8f5eb118fda5
zixuan zhang <zhangzix@usc.edu>
2019-11-24 16:47:13 -0800

    login and registration done and tested, working on channel and student/instructor class

--- code/src/main/java/Student.java
+++ code/src/main/java/Student.java
@@ -1,4 +1,31 @@
 
-public class Student {
-
+public class Student extends User{
+	public Student(String email, String password, String name) {
+		super(email, password, name);
+	}
+	
+	public void checkIn(String keyword, String ratings, double accuracy,
+							double latitude, double longitude, DBInterface db) {
+		// to do 
+	}
+	
+	public void askQuestion (String question,DBInterface db) {
+		// to do 
+	}
+	
+	public void addClass(String courseID, String info, DBInterface db) {
+		// to do 
+	} 
+	
+	public void dropClass(String courseID, DBInterface db) {
+		// to do 
+	}
+	
+	
+	public String getCourseList(DBInterface db) {
+		// to do 
+		return "";
+	}
+	
+	
 }
--- code/src/main/java/User.java
+++ code/src/main/java/User.java
@@ -1,4 +1,22 @@
+import java.io.Serializable;
 
-public class User {
-
+public abstract class User implements Serializable{
+	protected String email;
+	//protected DBInterface db;
+	protected String password;
+	protected String userID;
+	protected String name;
+	private static final long serialVersionUID = 1l;
+	public User (String email,String password, String name) {
+		this.email = email;
+		this.name = name;
+	}
+	
+	public abstract void addClass(String courseID, String info, DBInterface db);
+	public abstract void dropClass(String courseID, DBInterface db);
+	public abstract void checkIn(String keyword, String ratings, double accuracy,
+			double latitude, double longitude, DBInterface db);
+	public abstract String getCourseList(DBInterface db);
+	
+	
 }
